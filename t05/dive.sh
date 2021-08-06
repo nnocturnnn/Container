f2d1d862b8c577bec4b8d3c0aa31e61366a42a14
nnnoc <vikchehovich@gmail.com>
2021-08-06 23:57:30 +0300

    f

---
+++ t05/dive.sh
@@ -0,0 +1,2 @@
+curl https://gist.githubusercontent.com/fabripautasso/4fd44d683b4354d8d4deaebf100a90b4/raw/624732819439ff2856a1280af118c58169201a33/dive_initial_dockerfile > Dockerfile
+dive build - < Dockerfile
